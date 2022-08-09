48071016ff3a464b045a1b9c3741233dd7628526
Olivia <oliviatmanalastas@gmail.com>
2022-08-09 19:04:07 -0400

    Done with assignment

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot (arg = "Squawk!")
+   # puts will output the given phrase
+   puts arg
+   #return will return the phrase
+   arg
+end
